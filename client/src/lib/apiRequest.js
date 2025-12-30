908c2becddb3098874b4b24c5897d1c3b36bfb27
Chahak Tibdewal <tibdewalchahak1264@gmail.com>
2025-12-30 20:28:31 +0530

    updated frontend api

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL + "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
