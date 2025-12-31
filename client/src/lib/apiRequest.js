0a79c1eb7f1e11b993fd2fb6c6ad1a2ba2d63c98
Chahak Tibdewal <tibdewalchahak1264@gmail.com>
2025-12-31 16:12:18 +0530

    updates css

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
+  baseURL: (process.env.REACT_APP_API_URL || "http://localhost:8500") + "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
